b02f7fef93911db34cfdc332d1ef1c2d546216ca
Abdulmajid <abdulmajidhmishk@gmail.com>
2025-08-05 12:48:52 +0200

    RC-005 the Post ist erledigt

---
+++ routes/resources.js
@@ -0,0 +1,82 @@
+import express from 'express';
+import { readFileSync, writeFileSync } from 'fs';
+import path from 'path';
+import { fileURLToPath } from 'url';
+import { v4 as uuidv4 } from 'uuid';
+
+const router = express.Router();
+
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const data_file = path.join(__dirname, '../data', 'resources.json');
+
+
+router.get('/', (req, res) => {
+    try {
+        const data = readFileSync(data_file, 'utf8');
+        const resources = JSON.parse(data);
+        res.json(resources);
+    } catch (error) {
+        res.status(500).json({ error: 'Interner Serverfehler beim Laden der Ressourcen-Daten.' });
+    }
+});
+
+
+router.get('/:id', (req, res) => {
+    try {
+        const resourceId = req.params.id;
+        const data = readFileSync(data_file, 'utf8');
+        const resources = JSON.parse(data);
+        const resource = resources.find(r => r.id === resourceId);
+
+        if (resource) {
+            res.json(resource);
+        } else {
+            res.status(404).json({ error: `Ressource mit ID ${resourceId} nicht gefunden.` })
+        }
+
+    } catch (error) {
+        res.status(500).json({ error: 'Interner Serverfehler beim Laden der Ressourcen-Daten.' });
+    }
+});
+
+
+router.post('/', (req, res) => {
+    const newData = req.body;
+
+    if (!newData.title || !newData.type) {
+        res.status(400).json({ error: 'title und type sind erforderlich.' });
+        return;
+    }
+
+    // 1. Neues Resource Objekt
+
+    const newResource = {
+        id: uuidv4(),
+        ...newData
+    }
+
+    // 2. Vorhandene Daten aus der Datei lesen und in einem Array speicher.
+
+    try {
+        const data = readFileSync(data_file, 'utf8');
+        const resources = JSON.parse(data);
+
+         // 3. Das neue Objekt in das Array hinzufuegen.
+        resources.push(newResource);
+
+        // 4. Das neue Array in die Datei schreiben.
+        writeFileSync(data_file, JSON.stringify(resources, null, 2), 'utf8');
+        // 5. Antwort schicken
+        res.status(201).json(newResource);
+
+
+    } catch (error) {
+        res.status(500).json({ error: 'Interner Serverfehler bei der Verarbeitung der Ressourcen-Daten.' });
+    }
+
+});
+
+
+export default router;
--- server.js
+++ server.js
@@ -1,64 +1,14 @@
-import express from 'express'
-import { readFileSync } from 'fs'
-import path from 'path'
-import { fileURLToPath } from 'url';
+import express from 'express';
+import resourcesRouter from './routes/resources.js';
 
-
+const port = 5002;
 
 const app = express();
 
+app.use(express.json());
+app.use('/resources', resourcesRouter);
 
-const port = 5002;
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
-const data_file = path.join(__dirname, 'data', 'resources.json');
-
-
-app.get('/', (req, res) => {
-res.send('Hallo ich bin Start Seite')
-});
-
-app.get('/resources/:id' , (req, res) => {
- try {
-  const data = readFileSync(data_file, 'utf8');
-  const resources = JSON.parse(data);
-
-  const resourceId = req.params.id ;
-  const resource = resources.find(r=> r.id === resourceId);
-  if (resource)
-  {
-    res.json(resource);
-  } else {
-    res.status(404).json({error:` Hi Resources mit ID ${resourceId} nicht gefunden`})
-  }
-
- }catch(error){
-
-
- }
-})
-
-app.get('/resources', (req, res) => {
-try {
-  const data = readFileSync(data_file, 'utf8');
-  const resources = JSON.parse(data);
-  res.json(resources);
-
-} catch (error) {
-res.status(500).json({error:'Interner Serverfehler beim Laden der Daten'});
-}
-
-
-
-})
-
-app.get('/users', (req, res) => {
-
-  res.send('Hier kommen die Users spaeter.')
-})
 
 app.listen(port, () => {
-  console.log(`Server is running at http://localhost:${port}`);
-
+    console.log(`Server is running at http://localhost:${port}`);
 });
-
